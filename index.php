759861308654f15f098f738c58532726d0f7b59b
Arya Nair <aryaajitnair@gmail.com>
2023-09-28 14:39:35 +0530

    feat: Add MS SQL service, and mariaDB

--- index.php
+++ index.php
@@ -17,12 +17,28 @@
         }
         .parent>div{
             width:50vw;
+            border-style: solid;
+
         }
     </style>
 </head>
 <body>
     <?php
-        $db="sqlite";
+        $db="def";
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            if (isset($_POST['mysql'])) {
+                $db = "mysql";
+            } elseif (isset($_POST['postgres'])) {
+                $db = "pgsql";
+            } elseif (isset($_POST['sqlite'])) {
+                $db = "sqlite";
+            } elseif (isset($_POST['mssql'])) {
+                $db = "mssql";
+            } elseif (isset($_POST['mariaDB'])) {
+                $db = "mariaDB";
+            }
+        }
+        
         $status="Not connected";
         if($db=="mysql"){
             $servername = "localhost";
@@ -59,12 +75,40 @@
             } catch(PDOException $e) {
                 echo "Connection failed: " . $e->getMessage();
             }
+        }else if($db=="mssql"){
+            try{
+                $conn = new PDO("sqlsrv:Server=localhost;Database=master;Trusted_Connection=True;");
+                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                $status = "Connected successfully";
+            } catch(PDOException $e) {
+                echo "Connection failed: " . $e->getMessage();
+            }
+        }else if($db=="mariaDB"){
+            $servername = "localhost";
+            $username = "root";
+            $password = "password";
+            $dbname="class";
+            try {
+                $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username);
+                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                $status = "Connected successfully";
+            } catch(PDOException $e) {
+                echo "Connection failed: " . $e->getMessage();
+            }
         }
     ?>
 
-
+    
     <div class="parent">
-        <div>hello</div>
+        <div> 
+            <form method="post">
+                <button type="submit" name="mysql">mysql</button>
+                <button type="submit" name="postgres">postgres</button>
+                <button type="submit" name="sqlite">sqlite</button>
+                <button type="submit" name="mssql">mssql</button>
+                <button type="submit" name="mariaDB">maria db</button>
+            </form>
+        </div>
         <div>
             <h3>
             <?php echo $status; ?>
@@ -101,6 +145,18 @@
                 echo '$dbname="class";<br>
                 try{<br>
                     $conn = new PDO("sqlite::memory:");<br>
+                    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);<br>
+                    $status = "Connected successfully";<br>
+                } catch(PDOException $e) {<br>
+                    echo "Connection failed: " . $e->getMessage();<br>
+                }';
+            }else if($db=="mariaDB"){
+                echo '$servername = "localhost";<br><br>
+                $username = "root";<br>
+                $password = "password";<br>
+                $dbname="class";<br>
+                try {<br>
+                    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username);<br>
                     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);<br>
                     $status = "Connected successfully";<br>
                 } catch(PDOException $e) {<br>
